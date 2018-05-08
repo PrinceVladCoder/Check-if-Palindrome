ed0d5811d5374a35b03eee2f7e37d38d923caf7b
RDeminey <38655139+RDeminey@users.noreply.github.com>
2018-05-08 17:53:55 +0200

    Add files via upload

---
+++ CheckIfPalindrome/CheckIfPalindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CheckIfPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets check if a word is a Palindrome");
+            Console.WriteLine(" ");
+            Console.WriteLine("Please enter a word: ");
+            string userWord = Console.ReadLine();
+            Console.WriteLine(userWord);
+
+            char[] inputArray = userWord.ToCharArray();
+            Array.Reverse(inputArray);
+            string reverSed = new string(inputArray);
+
+            
+            if(userWord == reverSed)
+            {
+                Console.WriteLine("This word is a palindrome!");
+                Console.WriteLine(reverSed);
+            }
+            else if(userWord != reverSed)
+            {
+                Console.WriteLine("This word is not a palindrome");
+                Console.WriteLine(reverSed);
+            }
+        }
+    }
+}
